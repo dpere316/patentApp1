0b06bd7370246af242a269ac0e418f8743d8c448
mangose10 <leonar98@knights.ucf.edu>
2020-10-12 17:05:20 -0400

    get multiple users

--- backend/python/getMultipleUsers.py
+++ backend/python/getMultipleUsers.py
@@ -6,13 +6,13 @@ import mongo_connect
 
 def getUserAccounts():
  
-    res = {'array':[{}]}
-    for doc in mongo_connect.usrCol.find():
-     res.array.append(doc)
+    res = {'array':[]}
+    for i,doc in enumerate(mongo_connect.usrCol.find()):
+        res['array'].append(doc)
 
-    print(res)
     return res
 
 usr = str(getUserAccounts())
-json.loads(usr)
-print("parsing works")
+res = "{ \"express\": " + usr + " }"
+
+print(res.replace("\'", "\"").replace("ObjectId(", "").replace("\")", "\"").replace("\"response\": False", "\"response\": false"))
